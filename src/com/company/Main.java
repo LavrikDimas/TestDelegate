08228176bb9a476f5f7bb951db3f0ea0a39f3bb1
LavrikDimas <42786701+LavrikDimas@users.noreply.github.com>
2018-11-20 17:12:31 +0200

    Add BubbleSort

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,11 +6,16 @@ import java.util.function.Supplier;
 public class Main {
     public static  void  main(String[] args)
     {
+        /*
         int [] sumNumber = sumTwoNumbers((a, b) -> a + b);
 
         for (int i : sumNumber){
-            System.out.println(i);
+            //System.out.println(i);
         }
+        System.out.println("-------");
+        */
+        bubbleSort();
+
     }
 
     static int [] sumTwoNumbers(FunctInterfaces functInterfaces)
@@ -25,6 +30,25 @@ public class Main {
         }
 
         return returnSum;
+    }
+
+    static void bubbleSort(){
+        int [] masNum = new int[] {3, 4, 10, 22, 200, 6, 22};
+
+        for(int i = 0; i < masNum.length; i++){
+            for (int j = 0; j < masNum.length; j++){
+                if (masNum[i] < masNum[j])
+                {
+                    int swap = masNum[i];
+                    masNum[i] = masNum[j];
+                    masNum[j] = swap;
+                }
+            }
+        }
+
+        for (int n : masNum){
+            System.out.println(n);
+        }
     }
 
 }
